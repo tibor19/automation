b5ff7a933d4fe471a000cb4c545330e4b0c4f860
Tiberiu Covaci <tibi@covaci.se>
2017-10-22 16:37:16 +0100

    Using warning for reports

--- AutomationScripts/Scripts/Check-Comlpiance.ps1
+++ AutomationScripts/Scripts/Check-Comlpiance.ps1
@@ -27,25 +27,23 @@ catch {
     }
 }
 
-$sub = Get-AzureRmContext | Get-AzureRmSubscription
+$context = Get-AzureRmContext
+
+Write-Warning $context
 
 $scripts = Get-Module ComplianceScripts | Select -ExpandProperty ExportedFunctions | Select -ExpandProperty Keys
 
 foreach($script in $scripts){
-	Write-Output "Running script" $script
+	Write-Warning "Running script" $script
 
 	$result = &"$script" 
 		
 	if(($result -ne $null) -and (@($result).Count -gt 0)){
-		#$perScriptPath = Join-Path $rootFolder -ChildPath $script | Join-Path -ChildPath "$($subId).csv"
-		#$perSubscriptionPath = Join-Path -Path $subscriptionFolder -ChildPath "$($script).csv"
 		#$result | Export-Csv -NoTypeInformation -Path $perScriptPath
 		#$result | Export-Csv -NoTypeInformation -Path $perSubscriptionPath
 		Write-Output $result
 	}
 	else{
-		Write-Output "Nothing to report on $script for " $sub.Name
+		Write-Warning "Nothing to report on $script for " $context.Subscription.Name
 	}
 }
-
-Write-Output  ""
